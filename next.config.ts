b7d60752c1d648b495a197fc4f059b78595b6aa1
dalinnx1 <dalinnx1@gmail.com>
2025-04-19 13:52:24 +0300

    little

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+          config.resolve.alias.mongoose = false;
+        }
+        return config;
+      },
     images: {
         remotePatterns: [
             {
