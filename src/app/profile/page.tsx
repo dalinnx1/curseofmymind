cfd8a17a94746d20d2fb1fc86035af24d7519af8
dalinnx1 <dalinnx1@gmail.com>
2025-04-19 16:39:26 +0300

    deneme

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -56,6 +56,7 @@ export default function ProfilePage() {
     }
 
     if (!data?.user) redirect("/sign-in");
+    console.log(data?.user)
 
     return (
         <div className="flex flex-col md:flex-row min-h-screen">
